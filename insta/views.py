4021d4786c2e5259505c0748681acd8187d02aa9
sigilai <you@example.com>
2018-07-20 17:33:47 +0300

    added login and register form

--- insta/views.py
+++ insta/views.py
@@ -1,8 +1,13 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 
 def login_page(request):
 
-    return render(request, 'account/login.html')
+    return render(request, 'registration/login.html')
+
+@login_required(login_url='/accounts/login/')
+def image(request, image_id):
+    return  render(request,'registration/login.html')
 
--- instagram/urls.py
+++ instagram/urls.py
@@ -18,6 +18,7 @@ from django.conf.urls import url,include
 
 urlpatterns = [
 url(r'^admin/', admin.site.urls),
-    url(r'', include('insta.urls')),
+url(r'', include('insta.urls')),
+url(r'^accounts/', include('registration.backends.simple.urls'))
     
 ]
