fac662fa06aa691f4f72b65d305985f613953502
sigilai <you@example.com>
2018-07-21 22:46:14 +0300

    added login button

--- insta/urls.py
+++ insta/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import url,include
 from . import views
 
+
 urlpatterns =[
-    url('^$',views.login_page,name='login'),
+    url('^$',views.home,name='home'),
 ]
--- insta/views.py
+++ insta/views.py
@@ -3,11 +3,11 @@ from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 
-def login_page(request):
+def home(request):
 
-    return render(request, 'registration/login.html')
+    return render(request, 'home.html')
 
 @login_required(login_url='/accounts/login/')
-def image(request, image_id):
+def image(request, image_idz):
     return  render(request,'registration/login.html')
 
