1a1f5e958ae33820f831580362f9f7d26e94f345
sigilai <you@example.com>
2018-07-22 11:41:31 +0300

    added post image to models

--- insta/models.py
+++ insta/models.py
@@ -5,3 +5,4 @@ class Image(models.Model):
     image_name = models.CharField(max_length=30)
     image_caption = models.CharField(max_length=30)
     owner = models.CharField(max_length=20,default='Sigila')
+    post_image = models.ImageField(upload_to='post/',default='card')
