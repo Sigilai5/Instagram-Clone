58ba2ff16d14163795c0afe9d1d6e9871230f806
sigilai <you@example.com>
2018-07-22 13:18:47 +0300

    added nothing

--- insta/models.py
+++ insta/models.py
@@ -1,11 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 class Image(models.Model):
     image_name = models.CharField(max_length=30)
     image_caption = models.CharField(max_length=30)
-    owner = models.CharField(max_length=20,default='Sigila')
-    post_image = models.ImageField(upload_to='post/',default='card')
+    owner = models.ForeignKey(User, on_delete=models.CASCADE,default='Brian')
+    post_image = models.ImageField(upload_to='post/', default='card')
 
     def __str__(self):
         return self.image_name
