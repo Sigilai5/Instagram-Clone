b31c46c8be2e0bf531ab1dd7f4f337d706deafaf
sigilai <you@example.com>
2018-07-24 16:35:22 +0300

    removed sign up file

--- insta/forms.py
+++ insta/forms.py
@@ -1,6 +1,15 @@
 from .models import *
 from django import forms
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
 
+
+class SignupForm(UserCreationForm):
+    email = forms.EmailField(max_length=200,help_text='Required')
+
+    class Meta:
+        model = User
+        fields = ('username', 'email','password1','password2')
 
 class NewImageForm(forms.ModelForm):
     class Meta:
--- insta/urls.py
+++ insta/urls.py
@@ -7,7 +7,10 @@ from django.conf import settings
 urlpatterns =[
     url('^$',views.home,name='home'),
     url(r'^accounts/profile',views.profile,name='profile'),
-    url(r'^new/image$', views.new_image, name='new_image')
+    url(r'^new/image$', views.new_image, name='new_image'),
+    url(r'^signup/$',views.signup, name='signup'),
+    url(r'^activate/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$',
+        views.activate, name='activate'),
 
 ]
 
